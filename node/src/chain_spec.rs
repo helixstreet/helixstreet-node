de9626f542da725762b482accec28ab71afd3121
helixstreet <info@helixstreet.com>
2024-08-20 14:16:22 +0200

    nv

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -116,7 +116,7 @@ pub fn development_config() -> ChainSpec {
 pub fn local_testnet_config() -> ChainSpec {
 	// Give your base currency a unit name and decimal places
 	let mut properties = sc_chain_spec::Properties::new();
-	properties.insert("tokenSymbol".into(), "UNIT".into());
+	properties.insert("tokenSymbol".into(), "HLX".into());
 	properties.insert("tokenDecimals".into(), 12.into());
 	properties.insert("ss58Format".into(), 42.into());
 
