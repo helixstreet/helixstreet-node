87295042c984bfa3021186ebf50f877ba039ac88
helixstreet <info@helixstreet.com>
2024-08-25 20:42:53 +0200

    cli

--- node/src/command.rs
+++ node/src/command.rs
@@ -64,7 +64,7 @@ impl SubstrateCli for Cli {
 
 impl SubstrateCli for RelayChainCli {
 	fn impl_name() -> String {
-		"Parachain Collator Template".into()
+		"helixstreet node".into()
 	}
 
 	fn impl_version() -> String {
@@ -73,7 +73,7 @@ impl SubstrateCli for RelayChainCli {
 
 	fn description() -> String {
 		format!(
-			"Parachain Collator Template\n\nThe command-line arguments provided first will be \
+			"helixstreet collator node\n\nThe command-line arguments provided first will be \
 		passed to the parachain node, while the arguments provided after -- will be passed \
 		to the relay chain node.\n\n\
 		{} <parachain-args> -- <relay-chain-args>",
